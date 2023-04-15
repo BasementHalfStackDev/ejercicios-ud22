e33f54c61718b74e5c55a3179b02a52a83a2ece9
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-15 19:39:38 +0200

    Comment added

--- JPallas_TA22/src/main/java/JPallas/TA22/ejercicio1/ejercicio1_App.java
+++ JPallas_TA22/src/main/java/JPallas/TA22/ejercicio1/ejercicio1_App.java
@@ -6,6 +6,7 @@ import JPallas.TA22.ejercicio1.views.MainView;
 public class ejercicio1_App {
 
 	public static void main(String[] args) {
+		// Initialise program and controller
 		MainView view = new MainView();
 		MainViewController vcontroller = new MainViewController(view);
 
